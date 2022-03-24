c4f58052ef027642fe94ee294a48e721c8d2c6ba
Frank <frankperman03@gmail.com>
2022-03-24 12:14:58 -0600

    second commit

--- ejercicio8.php
+++ ejercicio8.php
@@ -4,3 +4,23 @@
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores aritméticos</title>
+</head>
+<body>
+
+    <form action="ejercicio8.php" method="post">
+
+
+
+    </form>
+    
+</body>
+</html>
