2232f5c119a81517e3e9acfb3c00be459fbc0846
Frank <frankperman03@gmail.com>
2022-04-07 12:00:58 -0500

    finish exercise 15

---
+++ ejercicio15.php
@@ -0,0 +1,18 @@
+<?php
+
+//FUNCIONES
+
+//function "nombre de function"(arg){}
+function imprimirNombre($nombre,$apellido){
+    //rutinas o instrucciones
+    echo "Hola " . $nombre . " " . $apellido . "<br/>";
+
+}
+
+//Se manda a llamar a la funcion y se le coloca el valor de argumento
+imprimirNombre("Franco","Pérez");
+imprimirNombre("Santiago","Pérez");
+imprimirNombre("Juan","Tierra");
+
+
+?>
