47a0a79be9c4f0246517b074ceab13f1fb5d5bd9
Frank <frankperman03@gmail.com>
2022-04-07 12:56:56 -0500

    Finish exercise 21

---
+++ ejercicio21.php
@@ -0,0 +1,14 @@
+<?php
+//Lectura de arreglos
+
+$frutas = array ("fresa","pera","higo");
+print_r($frutas);
+
+echo $frutas[0];
+
+for($indice=0; $indice<3; $indice++){
+
+    echo $frutas[$indice];
+}
+
+?>
